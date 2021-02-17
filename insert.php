7911d98c6c0ee3767da974d6cbe7fe0e2873bf0e
Kulanan Ruanthip <78783416+kulanangit@users.noreply.github.com>
2021-02-17 21:31:27 +0700

    change pattern and check username before insert db

--- insert.php
+++ insert.php
@@ -1,23 +1,39 @@
 <?php
-$conn=mysqli_connect("localhost", "root", "","Student");
+$conn=mysqli_connect("localhost", "root", "","helloboard_db");
 $conn->query("SET NAMES UTF8");
  
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
-// sql to delete a record
-$sql = "INSERT INTO Profile (user_id, username, password, role, image, major_id) 
-VALUES (null,'".$_POST['username']."','".$_POST['password']."',0,'".$_POST['img2']."','".$_POST['major']."')";
-if ($conn->query($sql) === TRUE) {
-  echo "Insert Successfully!!";
 } else {
-  echo "Error Insert record: " . $conn->error;
+
+$sql1 = "SELECT * FROM user WHERE username = '".$_POST['username']."'";
+$result = mysqli_query($conn, $sql1);
+
+if(mysqli_num_rows($result) == 0){ //dont have this username then insert
+
+$sql = "INSERT INTO user (user_id, username, password, role, image, major_id) 
+VALUES (null,'".$_POST['username']."','".$_POST['password']."',0,'".$_POST['avatar']."','".$_POST['major']."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "<script 'text/JavaScript'>";
+  echo "alert('sign up successful!');";
+  echo "</script>";
+  echo "<meta http-equiv='refresh' content='0;URL=login.htm'>"; //redirect page
+
+} else {
+  echo "<script 'text/JavaScript'>";
+  echo "alert('error!');";
+  echo "</script>";
+  echo "<meta http-equiv='refresh' content='0;URL=signup.htm'>";
 };
 
+}else{ 
+  echo "<script 'text/JavaScript'>";
+  echo "alert('This username has been taken!');";
+  echo "</script>";
+  echo "<meta http-equiv='refresh' content='0;URL=signup.htm'>";
+}
+}
 $conn->close();
 ?>
-<html>
-<br><br>
-<a href="lab1.htm">Go to Home</a>
-</html>
