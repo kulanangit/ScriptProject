404261eb5fef23241fd73456461b55c395aa1a54
anawat28 <anawatbongprom@gmail.com>
2021-02-18 18:13:09 +0700

    new question

--- NewQuestion.php
+++ NewQuestion.php
@@ -1,28 +1,44 @@
 <html>
 <head>
   <title>ThaiCreate.Com</title>
-  <link rel="stylesheet" type="text/css" href="NewQuestion.css"> 
+  <link rel="stylesheet" type="text/css" href="NewQuestion.css" > 
 </head>
 <body>
   <form action="insertQuestion.php" method="post" name="frmMain" id="frmMain">
     
-  <h3>Add new post</h3>
+  <h3 class="addnew" >Add new post</h3>
   <div >
-        <div class="form-group">
+    <table align="center" >
+      <tr>
+          <td>
               <p1>Question</p1>
+          </td>
+          <td>
               <p2><input name="txtQuestion" type="text" id="txtQuestion" value="" size="55"></p2>
-        </div>
-        <br/>
-        <div class="form-group">
-          <p1 >Details  </p1>
-          <p2><textarea name="txtDetails" cols="50" rows="5" id="txtDetails"></textarea></p2>
-        </div>
-        <br/>
-        <div class="form-group">
-          <p1 >Name</p1>
-          <p2 ><input name="txtName" type="text" id="txtName" value="" size="55"></p2>
-        </div>
-    <br><input name="btnSave" type="submit" id="btnSave" value="Submit">
+          </td>
+      </tr>
+      
+      <tr>
+          <td>
+              <p1 >Details  </p1>
+          </td>
+          <td>
+              <p2><textarea name="txtDetails" cols="50" rows="5" id="txtDetails"></textarea></p2>
+          </td>
+      </tr>
+      <tr>
+          <td>
+            <p1 >Name</p1>
+          </td>
+          <td>  
+            <p2 ><input name="txtName" type="text" id="txtName" value="" size="55"></p2>
+          </td>
+      </tr>        
+    </table>
+    <br>
+    <div class="input-button" align="center">
+            <input name="btnSave" type="submit" id="btnSave" value="Submit">
+    <div>
   </form>
 </body>
 </html>
