ca3c6473b32a69949d00f30046383deb8ad7b203
Chavy <nadan42@hotmail.com>
2021-02-19 21:25:35 +0700

    v

--- Webboard.php
+++ Webboard.php
@@ -48,6 +48,7 @@ $(document).ready(function(){
 </head>
 <body>
   <div class="navbar">
+    
     <a href="Webboard.php">Public</a>
     <div class="subnav">
       <button class="subnavbtn">Major<i class="fa fa-caret-down"></i></button>
@@ -58,26 +59,8 @@ $(document).ready(function(){
       <a href="Webboard_Engineer.php">Engineering</a>
       <a href="Webboard_Medicine.php">Medicine</a>
       </div>
+      <label class="lg"><a href="logout.php" >Logout</a></label>
     </div> 
-    <div class="subnav">
-      <button class="subnavbtn">Services <i class="fa fa-caret-down"></i></button>
-      <div class="subnav-content">
-        <a href="#bring">Bring</a>
-        <a href="#deliver">Deliver</a>
-        <a href="#package">Package</a>
-        <a href="#express">Express</a>
-      </div>
-    </div> 
-    <div class="subnav">
-      <button class="subnavbtn">Partners <i class="fa fa-caret-down"></i></button>
-      <div class="subnav-content">
-       <li> <a href="#link1">Link 1</a></li>
-        <a href="#link2">Link 2</a>
-        <a href="#link3">Link 3</a>
-        <a href="#link4">Link 4</a>
-      </div>
-    </div>
-    <a href="logout.php" div="ltl">Logout</a>
   </div>
 
 
@@ -87,7 +70,7 @@ $(document).ready(function(){
 <form name="frmSearch" method="get" action="<?php echo $_SERVER['SCRIPT_NAME'];?>">
 <div class="container">
    <br />
-   <h2 class="header">SUT webboard</h2><br />
+   <h2 class="header">SUT Webboard</h2><br />
    
   <div class="form-group">
     <div class="input-group">
