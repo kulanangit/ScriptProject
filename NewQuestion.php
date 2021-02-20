fb343e10b2b3e819930de9794092f21062217f7e
B6121853 <amp.nonthawan.g7@gmail.com>
2021-02-21 04:10:43 +0700

    Edit Insert Question

--- NewQuestion.php
+++ NewQuestion.php
@@ -52,9 +52,45 @@ session_start();
           <td>  
             <p2><input name="txtName" type="text" id="txtName" value="<?php echo $_SESSION['username'] ?>" size="55" placeholder="Insert your avatar name...." disabled></p2>
           </td>
-      </tr>        
+      </tr> 
+      <tr>
+          <td> Topic</td>
+          <td>
+          <div class="box">
+            <select name="topic">
+              <option disabled selected>Choose Topic...</option>
+              <option value="Love">Love</option>
+              <option value="Education">Education</option>
+              <option value="Drama">Drama</option>
+              <option value="Health">Health</option>
+              <option value="Game">Game</option>
+              <option value="Idol">Idol</option>
+            </select>
+          </div>
+          </td>
+      </tr>
+      <tr>
+          <td> Major</td>
+          <td>
+          <div class="box">
+            <select name="major">
+              <option disabled selected>Choose Major...</option>
+              <option value="Science">Science</option>
+              <option value="Social Technology">Social Technology</option>
+              <option value="Agriculture">Agriculture</option>
+              <option value="Medicine">Medicine</option>
+              <option value="Nurse">Nurse</option>
+              <option value="Engineer">Engineer</option>
+              <option value="Dentisty">Dentisty</option>
+              <option value="Public Health">Public Health</option>
+            </select>
+            </div>
+          </td>
+      </tr>       
     </table>
     <br>
+    <br>
+     <br>
     <div class="input-button" align="center">
             <input name="btnSave" type="submit" id="btnSave" value="Submit" <?php if($_SESSION['username'] == "")  {echo "disabled=\"disabled\"";} ?>>
     <div>
--- insertQuestion.php
+++ insertQuestion.php
@@ -7,7 +7,7 @@ $conn=mysqli_connect("localhost", "root", "","helloboard_db");
 date_default_timezone_set("Asia/Bangkok"); //set time zone
 $date = date("Y-m-d H:i:s", time());
 
-	$sql = "INSERT INTO webboard(CreateDate,Question,Details,Name) VALUES('".$date."','".$_POST["txtQuestion"]."','".$_POST["txtDetails"]."','".$_SESSION['username']."')";
+	$sql = "INSERT INTO webboard(QuestionID,CreateDate,Question,Details,Name,Major,Category) VALUES('','".$date."','".$_POST["txtQuestion"]."','".$_POST["txtDetails"]."','".$_SESSION['username']."','".$_POST["major"]."','".$_POST["topic"]."')";
 	$rs = mysqli_query($conn,$sql);
 	
 	if($rs){
