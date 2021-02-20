5726f6c5075f57636d68010c87012e3a076b8036
Kulanan Ruanthip <78783416+kulanangit@users.noreply.github.com>
2021-02-20 15:41:54 +0700

    hide error

--- editForm.php
+++ editForm.php
@@ -3,6 +3,10 @@
 session_start();
 $conn=mysqli_connect("localhost", "root", "","helloboard_db");
 $conn->query("SET NAMES UTF8");
+
+error_reporting(0);
+ini_set('display_errors', 0); //hide error
+
         if($_SESSION['username'] == ""){
             echo "<center>Please Login!<center>";
         } 
