f921eafe0aee1a514587f54f93b72d8e4892618e
Kulanan Ruanthip <78783416+kulanangit@users.noreply.github.com>
2021-02-21 12:07:09 +0700

    update view and delete user

--- delete_user.php
+++ delete_user.php
@@ -1,24 +1,23 @@
 <?php
 $conn=mysqli_connect("localhost", "root", "","helloboard_db");
 $conn->query("SET NAMES UTF8");
- 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 } else {
 // sql to delete a record
-$sql = "DELETE FROM user WHERE user_id='".$_GET['user_id']."'";
+$sql = "DELETE FROM user WHERE username ='".$_GET['username']."'";
 
 if ($conn->query($sql) === TRUE) {
   echo "<script 'text/JavaScript'>";
-  echo "alert('user ID:".$_GET['user_id']." has been deleted!!');";
+  echo "alert('user ID: '".$_GET['username']."' has been deleted!!');";
   echo "</script>";
-  echo "<meta http-equiv='refresh' content='0; URL=admin.php'>";
+  echo "<meta http-equiv='refresh' content='0; URL=list_user.php'>";
 } else {
     echo "<script 'text/JavaScript'>";
     echo "alert('user ID: Error deleting record: " . $conn->error;
     echo "</script>";
-    echo "<meta http-equiv='refresh' content='0; URL=admin.php'>";
+    echo "<meta http-equiv='refresh' content='0; URL=list_user.php'>";
 }
 
 $conn->close();
